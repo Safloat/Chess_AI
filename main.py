d365dc73113578b0c796641269538097a6972d48
Safloat <safaknaim99@gmail.com>
2021-05-29 17:59:59 +0500

    Co-authored-by: 444iamahmed <444iamahmed@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame as p
+import game
+
+
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+
+MAX_FPS = 30
+
+IMAGES = {}
+
+def loadImages():
+
+    print("got here")
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png")) 
+    
+
+def main():
+
+    p.init()
+    print("inited")
+    screen = p.display.set_mode()
+    clock = p.time.Clock()
+
+    screen.fill(p.Color("white"))
+
+    game_state = game.get_state()
+    #loadImages()
+    running = True
+
+    while running:
+        
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, game_state)
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, game_state):
+    drawBoard(screen)
+
+    #drawPieces(screen, game_state)
+
+def drawBoard(screen):
+
+    colors = [p.Color("gray"), p.Color("black")]
+
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+main()
---
+++ pieces.py
@@ -0,0 +1,13 @@
+from enum import Enum
+class piece(Enum):
+
+    none = 0
+    queen = 1
+    king = 2
+    knight = 3
+    bishop = 4
+    rook = 5
+    queen = 7
+
+    white = 8
+    black = 16
